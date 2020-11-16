3898ded040ac99af7f4771b4692dda14ea82241e
shtef21 <72214442+shtef21@users.noreply.github.com>
2020-11-16 14:07:45 +0100

    Gumb "Click me"

--- project Muller Salopek/project Muller Salopek/Form1.cs
+++ project Muller Salopek/project Muller Salopek/Form1.cs
@@ -16,5 +16,10 @@ namespace project_Muller_Salopek
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
