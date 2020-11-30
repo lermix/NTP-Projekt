37dbb22527b2a98bdbc984f12ea397627576dae5
lermix <61510304+lermix@users.noreply.github.com>
2020-11-30 12:49:06 +0100

    xmlManager, AllArticlesXML, neke osnovne funkcije sto se dogodi kad kliknemo na artikl

---
+++ project Muller Salopek/project Muller Salopek/XmlManager.cs
@@ -0,0 +1,43 @@
+﻿using project_Muller_Salopek.dataObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+
+namespace project_Muller_Salopek
+{
+
+    //Bavi se xml fileovima
+    public class XmlManager
+    {
+        static List<Article> allArticles = new List<Article>();
+
+        //NIJE DOVRSENO
+        XDocument AllArticlesXML = XDocument.Load(@"assets\Data\AllArticlesXML.xml");
+        
+
+        //Dohvaca artikl prema buttonName atributu iz AllArticlesXML -- NIJE DOVRSENO
+        public static List<Article> GetArticles()
+        {
+            return allArticles;
+        }
+
+        //Dodaje artikl u AllArticlesXML --- NIJE DOVRSENO
+        public void AddArticle()
+        {
+            Article a = new Article(0, "test", "Article1", 5);
+            XmlSerializer serializer = new XmlSerializer(a.GetType());
+            serializer.Serialize(Console.Out, a);
+        }
+
+
+
+
+    }
+}
--- project Muller Salopek/project Muller Salopek/dataObjects/Article.cs
+++ project Muller Salopek/project Muller Salopek/dataObjects/Article.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace project_Muller_Salopek.dataObjects
 {
+    [Serializable]
+    [XmlRoot("Article")]
     public class Article
     {
-        int ID;
-        string name, buttonName;
-        float price;
+        [XmlElement("ID")]
+        public int ID { get; set; }
+        [XmlElement("name")]
+        public string name { get; set; }
+        [XmlElement("buttonName")]
+        public string buttonName { get; set; }
+        [XmlElement("price")]
+        public float price { get; set; }
+
+        public Article (int _ID, string _name, string _buttonName, float _price)
+        {
+            ID = _ID;
+            name = _name;
+            buttonName = _buttonName;
+            price = _price;
+        }
+        public Article ()
+        { }
+
+        public bool checkButtonName(string buttonNameToCompare)
+        {
+            return (buttonName == buttonNameToCompare) ? true : false;
+        }
+
     }
 }
