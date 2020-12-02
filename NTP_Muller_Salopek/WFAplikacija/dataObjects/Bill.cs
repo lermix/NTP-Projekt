f8c374505ec0e03d82bf85cbdc810f274a347e9a
lermix <61510304+lermix@users.noreply.github.com>
2020-12-02 12:57:29 +0100

    user, bill class

---
+++ NTP_Muller_Salopek/WFAplikacija/dataObjects/Bill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_Muller_Salopek.dataObjects
+{
+    class Bill
+    {
+        public DateTime dateTime { get; set; }
+        public List<Article> aeticles { get; set; }
+        public User user { get; set; }
+        public int totalPrice { get; set; }
+
+    }
+}
